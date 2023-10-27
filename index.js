9edc67580238adeec65d851dd7c47429394e73d5
Harmony Chofor <jethroharmony11@gmail.com>
2023-10-27 10:15:28 -0500

    week4.

---
+++ index.js
@@ -0,0 +1,18 @@
+/*
+let basePrice = 0;
+let taxAmount =0;
+let totalDue =0;
+const TAX_RATE - 0.0825;
+
+function sumitorder(
+    scoopCount,
+     containertyp,
+      hasSprinkles,
+       hasFudge, 
+       hasWhip, 
+       hasCherry
+){*/
+submitOederButton.onclick =()=>{
+    const selectcontainertype =document.querySelector("input")
+}
+}
